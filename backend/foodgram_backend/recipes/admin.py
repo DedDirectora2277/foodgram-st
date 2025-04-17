0892eae03b8b618d126ed3f869d99115411e7a25
Иван Демин <wert227752@gmail.com>
2025-04-17 14:38:35 +0700

    Adding Ingredient model

--- backend/foodgram_backend/recipes/admin.py
+++ backend/foodgram_backend/recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    """Админ-панель для модели Ingredient"""
+
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+    list_filter = ('measurement_unit',)
+    empty_value_display = '-пусто-'
+
--- backend/foodgram_backend/recipes/models.py
+++ backend/foodgram_backend/recipes/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """Модель ингредиента"""
+
+    name = models.CharField(
+        verbose_name='Название ингредиента',
+        max_length=200,
+        db_index=True,
+    )
+
+    measurement_unit = models.CharField(
+        verbose_name='Единица измерения',
+        max_length=50,
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = ('name',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'measurement_unit'],
+                name='unique_ingredient_unit'
+            )
+        ] # Гарантия уникальности пары Название + Ед. измер.
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'

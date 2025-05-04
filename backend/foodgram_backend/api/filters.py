ffb70ca50b5356ee66f338ba1ccd067db8beda83
Иван Демин <wert227752@gmail.com>
2025-05-05 00:17:21 +0700

    Changing author filter in RecipeFilter

--- backend/foodgram_backend/api/filters.py
+++ backend/foodgram_backend/api/filters.py
@@ -1,17 +1,24 @@
-from django_filters import rest_framework as filters
+import django_filters
 from recipes.models import Recipe
 from rest_framework import filters as rest_filters
 
+from users.models import User
 
-class RecipeFilter(filters.FilterSet):
+
+class RecipeFilter(django_filters.rest_framework.FilterSet):
     """
     Фильтры для модели Recipe.
     Позволяет фильтровать по автору, статусу в избранном и списке покупок
     """
 
-    author = filters.NumberFilter(field_name='author__id')
-    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(
+    author = django_filters.rest_framework.ModelChoiceFilter(
+        queryset=User.objects.all(),
+        field_name='author'
+    )
+    is_favorited = django_filters.rest_framework.BooleanFilter(
+        method='filter_is_favorited'
+    )
+    is_in_shopping_cart = django_filters.rest_framework.BooleanFilter(
         method='filter_is_in_shopping_cart'
     )
 

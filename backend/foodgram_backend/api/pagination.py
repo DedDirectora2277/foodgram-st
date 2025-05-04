180afbce7cc2db50c7c1f929da6d82a2920169f3
Иван Демин <wert227752@gmail.com>
2025-05-05 00:28:56 +0700

    Adding pagination constants and refactoring custom permission class

--- backend/foodgram_backend/api/pagination.py
+++ backend/foodgram_backend/api/pagination.py
@@ -1,8 +1,13 @@
 from rest_framework.pagination import PageNumberPagination
 
+from constants import (
+    DEFAULT_PAGE_SIZE,
+    MAX_PAGE_SIZE
+)
+
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 10
+    page_size = DEFAULT_PAGE_SIZE
     page_query_param = 'page'
     page_size_query_param = 'limit'
-    max_page_size = 100
+    max_page_size = MAX_PAGE_SIZE
--- backend/foodgram_backend/api/permissions.py
+++ backend/foodgram_backend/api/permissions.py
@@ -9,14 +9,14 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
 
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return request.user and request.user.is_authenticated
+        return (
+            request.user and request.user.is_authenticated
+            or request.method in permissions.SAFE_METHODS
+        )
 
     def has_object_permission(self, request, view, obj):
 
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return obj.author == request.user
+        return (
+            obj.author == request.user
+            or request.method in permissions.SAFE_METHODS
+        )
